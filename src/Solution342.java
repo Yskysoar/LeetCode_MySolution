57e3bd90ca5f9c50c45bcfc43aa7fa3ccce9f8ef
Y_skysoar <2734198149@qq.com>
2025-08-15 00:26:06 +0800

    2025.8.15LeetCode

---
+++ src/Solution342.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+/**
+ * @author Yskysoar
+ * @createTime 2025-08-15 0:12
+ * @description 342. 4的幂
+ * 给定一个整数，写一个函数来判断它是否是 4 的幂次方。如果是，返回 true ；否则，返回 false 。
+ * 整数 n 是 4 的幂次方需满足：存在整数 x 使得 n == 4^x
+ * 示例 1：
+ * 输入：n = 16
+ * 输出：true
+ * 示例 2：
+ * 输入：n = 5
+ * 输出：false
+ * 示例 3：
+ * 输入：n = 1
+ * 输出：true
+ * 提示：
+ * -2^31 <= n <= 2^31 - 1
+ */
+public class Solution342 {
+    public ArrayList<Integer> arrayList = calculate();
+
+    public ArrayList<Integer> calculate() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 16; i++) {
+            arrayList.add((int) Math.pow(4, i));
+        }
+        return arrayList;
+    }
+
+    public boolean isPowerOfFour(int n) {
+        return arrayList.contains(n);
+    }
+}
