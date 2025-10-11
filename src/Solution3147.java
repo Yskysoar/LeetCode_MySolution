a4ae30a0c12ee5b2299fc602fc7757bdec9d2929
Y_skysoar <2734198149@qq.com>
2025-10-11 10:50:30 +0800

    2025.10.10LeetCode

---
+++ src/Solution3147.java
@@ -0,0 +1,48 @@
+/**
+ * @author Yskysoar
+ * @createTime 2025-10-11 10:29
+ * @description 3147. 从魔法师身上吸取的最大能量
+ * 在神秘的地牢中，n 个魔法师站成一排。每个魔法师都拥有一个属性，这个属性可以给你提供能量。有些魔法师可能会给你负能量，即从你身上吸取能量。
+ * 你被施加了一种诅咒，当你从魔法师 i 处吸收能量后，你将被立即传送到魔法师 (i + k) 处。这一过程将重复进行，直到你到达一个不存在 (i + k) 的魔法师为止。
+ * 换句话说，你将选择一个起点，然后以 k 为间隔跳跃，直到到达魔法师序列的末端，在过程中吸收所有的能量。
+ * 给定一个数组 energy 和一个整数k，返回你能获得的 最大 能量。
+ * 示例 1：
+ * 输入： energy = [5,2,-10,-5,1], k = 3
+ * 输出： 3
+ * 解释：可以从魔法师 1 开始，吸收能量 2 + 1 = 3。
+ * 示例 2：
+ * 输入： energy = [-2,-3,-1], k = 2
+ * 输出： -1
+ * 解释：可以从魔法师 2 开始，吸收能量 -1。
+ * 提示：
+ * 1 <= energy.length <= 10^5
+ * -1000 <= energy[i] <= 1000
+ * 1 <= k <= energy.length - 1
+ */
+public class Solution3147 {
+    public static void main(String[] args) {
+        Solution3147 solution3147 = new Solution3147();
+        int ans = solution3147.maximumEnergy(new int[]{5, 2, -10, -5, 1}, 3);
+        System.out.println(ans);
+    }
+
+    /**
+     * 贪心 + 倒序
+     * 因为起点不定，但是终点固定,只有[energy.length - k,energy.length - 1]闭区间范围内
+     * 所以从终点往回推，由于元素正负不定，记录出现的最大值即为答案
+     * @param energy 数据数组
+     * @param k      跳跃步长
+     * @return 可以获取的最大值
+     */
+    public int maximumEnergy(int[] energy, int k) {
+        int ans = Integer.MIN_VALUE;
+        for (int i = energy.length - 1; i >= energy.length - k; i--) {
+            int sum = 0;
+            for (int j = 0; i - j >= 0; j += k) {
+                sum += energy[i - j];
+                ans = Math.max(ans, sum);
+            }
+        }
+        return ans;
+    }
+}
