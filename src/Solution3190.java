7dd5caae6e504efaf9e217294b5fc55ada8d1b00
Y_skysoar <2734198149@qq.com>
2025-11-22 09:08:23 +0800

    2025.11.22LeetCode

---
+++ src/Solution3190.java
@@ -0,0 +1,30 @@
+/**
+ * @author Yskysoar
+ * @createTime 2025-11-22 9:06
+ * @description 3190. 使所有元素都可以被 3 整除的最少操作数
+ * 给你一个整数数组 nums 。一次操作中，你可以将 nums 中的 任意 一个元素增加或者减少 1 。
+ * 请你返回将 nums 中所有元素都可以被 3 整除的 最少 操作次数。
+ * 示例 1：
+ * 输入：nums = [1,2,3,4]
+ * 输出：3
+ * 解释：
+ * 通过以下 3 个操作，数组中的所有元素都可以被 3 整除：
+ * 将 1 减少 1 。
+ * 将 2 增加 1 。
+ * 将 4 减少 1 。
+ * 示例 2：
+ * 输入：nums = [3,6,9]
+ * 输出：0
+ * 提示：
+ * 1 <= nums.length <= 50
+ * 1 <= nums[i] <= 50
+ */
+public class Solution3190 {
+    public int minimumOperations(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {//取余结果非1即2，1即减1，2即加1，都只需要操作一步即可
+            if (num % 3 != 0) ans++;
+        }
+        return ans;
+    }
+}
